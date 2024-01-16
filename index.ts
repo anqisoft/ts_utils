5ea783b35f5a3ca736a3504786e6daab8a160f24
anqisoft <anqisoft@gmail.com>
2024-01-16 20:54:03 +0800

    update index.d.ts

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@
  * index.ts
  *
  * <en_us>
- * Creation: January 15, 2024 11:38:05
- * Function: Provide the most basic and common feature of TypeScript code libraries, which is paired with the javascript version of js_utils library, etc.
- * </en_us>
+  * Creation: January 15, 2024 11:38:05
+  * Function: Provide the most basic and common feature of TypeScript code libraries, which is paired with the javascript version of js_utils library, etc.
+  * </en_us>
  *
  * <zh_cn>
  * 创建：2024年1月15日 11:38:05
@@ -21,47 +21,47 @@
 /// <references="./types/lib.deno.ns.d.ts"
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>characters: provide special characters, such as changing rows, replacement divisions, etc.</en_us>
 <zh_cn>字符：提供特殊字符，如换行符、替换分隔符等</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>字符：提供特殊字符，如換行符、替換分隔符等</zh_tw>
 ================================================================================================ */
 /**
-    <en_us>en_us</en_us>
+    <en_us>Enter</en_us>
     <zh_cn>回车</zh_cn>
-    <zh_tw>zh_tw</zh_tw>
+    <zh_tw>回車</zh_tw>
 */
 export const CR = '\r';
 
 /**
-    <en_us>en_us</en_us>
+    <en_us>change line</en_us>
     <zh_cn>换行</zh_cn>
-    <zh_tw>zh_tw</zh_tw>
+    <zh_tw>換行</zh_tw>
 */
 export const LF = '\n';
 
 /**
-    <en_us>en_us</en_us>
+    <en_us>Enter the row symbol</en_us>
     <zh_cn>回车换行符</zh_cn>
-    <zh_tw>zh_tw</zh_tw>
+    <zh_tw>回車換行符</zh_tw>
 */
 export const CRLF = '\r\n';
 
 /**
-    <en_us>en_us</en_us>
+    <en_us>split dedicated separators, first replace a series of special string with this, and then use this further split</en_us>
     <zh_cn>拆分专用分隔符，先把一系列特殊的字符串替换成这个，再用这个进一步拆分</zh_cn>
-    <zh_tw>zh_tw</zh_tw>
+    <zh_tw>拆分專用分隔符，先把一系列特殊的字符串替換成這個，再用這個進一步拆分</zh_tw>
 */
 export const SEPARATOR_OF_SPLIT = '\0';
 
 declare global {
 	interface String {
 		/**
-            <en_us>en_us</en_us>
+            <en_us>The specified string word or regular expression is replaced with a dedicated separator</en_us>
             <zh_cn>将指定字符串字面量或正则表达式，替换为拆分专用分隔符</zh_cn>
-            <zh_tw>zh_tw</zh_tw>
+            <zh_tw>將指定字符串字面量或正則表達式，替換為拆分專用分隔符</zh_tw>
 
-            @param from {string | RegExp} <en_us>en_us</en_us><zh_cn>想要替换为拆分专用分隔符的字符串字面量或正则表达式</zh_cn><zh_tw>zh_tw</zh_tw>
-            @returns {string} <en_us>en_us</en_us><zh_cn>替换结果</zh_cn><zh_tw>zh_tw</zh_tw>
+            @param from {string | RegExp} <en_us>Want to replace it with a string of string or regular expression of the dedicated separator</en_us><zh_cn>想要替换为拆分专用分隔符的字符串字面量或正则表达式</zh_cn><zh_tw>想要替換為拆分專用分隔符的字符串字面量或正則表達式</zh_tw>
+            @returns {string} <en_us>replacement result</en_us><zh_cn>替换结果</zh_cn><zh_tw>替換結果</zh_tw>
         */
 		_replaceToSplitSeparator(this: string, from: string | RegExp): string;
 	}
@@ -72,24 +72,24 @@ String.prototype._replaceToSplitSeparator = function (from: string | RegExp): st
 };
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>Date, such as providing GMT+8 related methods in China</en_us>
 <zh_cn>日期，如提供中国时区GMT+8相关方法</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>日期，如提供中國時區GMT+8相關方法</zh_tw>
 ================================================================================================ */
 /**
- * <en_us>date correction value</en_us>
+ * <en_us>Date correction value</en_us>
  * <zh_cn>日期修正值</zh_cn>
  * <zh_tw>日期修正值</zh_tw>
  */
 export const DATE_OFFSET_GMT8 = 1000 * 60 * 60 * 8;
 
 /**
- * <en_us>Get the corrected date</en_us>
+ * <en_us>Get the revised date</en_us>
  * <zh_cn>获取修正后的日期</zh_cn>
- * <zh_tw>取得修正後的日期</zh_tw>
+ * <zh_tw>獲取修正後的日期</zh_tw>
  *
  * @returns
- * <en_us>the corrected date</en_us>
+ * <en_us>The date after correction</en_us>
  * <zh_cn>修正后的日期</zh_cn>
  * <zh_tw>修正後的日期</zh_tw>
  */
@@ -98,12 +98,12 @@ export function getFixedDate(): Date {
 }
 
 /**
- * <en_us>Get the corrected date</en_us>
+ * <en_us>Get the revised date</en_us>
  * <zh_cn>获取修正后的日期</zh_cn>
- * <zh_tw>取得修正後的日期</zh_tw>
+ * <zh_tw>獲取修正後的日期</zh_tw>
  *
  * @returns
- * <en_us>the corrected date</en_us>
+ * <en_us>The date after correction</en_us>
  * <zh_cn>修正后的日期</zh_cn>
  * <zh_tw>修正後的日期</zh_tw>
  */
@@ -117,19 +117,19 @@ export function getFilenameTimestampPostfix(): string {
 declare global {
 	interface Date {
 		/**
-		 * <en_us>en_us</en_us><zh_cn>获取增加中国GMT+8时区修正值的日期</zh_cn><zh_tw>zh_tw</zh_tw>
+		 * <en_us>Get the date of increasing the revision of China's GMT+8 time zone</en_us><zh_cn>获取增加中国GMT+8时区修正值的日期</zh_cn><zh_tw>獲取增加中國GMT+8時區修正值的日期</zh_tw>
 		 * @returns {Date} 加了中国GMT+8时区修正值的日期
 		 */
 		_addGmt8(): Date;
 
 		/**
-		 * <en_us>en_us</en_us><zh_cn>转为文件名时间戳后缀字符串</zh_cn><zh_tw>zh_tw</zh_tw>
+		 * <en_us>turn to file name timestamp vectoring string</en_us><zh_cn>转为文件名时间戳后缀字符串</zh_cn><zh_tw>轉為文件名時間戳後綴字符串</zh_tw>
 		 * @returns {string} 文件名时间戳后缀
 		 */
 		_toFilenameTimestampPostfix(): string;
 
 		/**
-		 * <en_us>en_us</en_us><zh_cn>增加中国GMT+8时区修正值，再转文件名时间戳后缀字符串</zh_cn><zh_tw>zh_tw</zh_tw>
+		 * <en_us>increase the correction value of China's GMT+8 time zone, and then turn the file name timestamp vesarian string</en_us><zh_cn>增加中国GMT+8时区修正值，再转文件名时间戳后缀字符串</zh_cn><zh_tw>增加中國GMT+8時區修正值，再轉文件名時間戳後綴字符串</zh_tw>
 		 * @returns {string} 加了中国GMT+8时区修正值后，所转的文件名时间戳后缀
 		 */
 		_toGmt8FilenameTimestampPostfix(): string;
@@ -156,54 +156,54 @@ Date.prototype._toGmt8FilenameTimestampPostfix = function (): string {
 };
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>Method Aid: Provide a method of assisting methods, such as step methods, asynchronous method interfaces and corresponding guards</en_us>
 <zh_cn>方法辅助：提供方法的辅助方法，如同步方法、异步方法接口及相应守卫</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>方法輔助：提供方法的輔助方法，如同步方法、異步方法接口及相應守衛</zh_tw>
 ================================================================================================ */
 /**
- * <en_us>Interface: synchronization method</en_us>
+ * <en_us>interface: synchronization method</en_us>
  * <zh_cn>接口：同步方法</zh_cn>
- * <zh_tw>介面：同步方法</zh_tw>
+ * <zh_tw>接口：同步方法</zh_tw>
  */
 export interface SyncFunc extends Function {
 	(...args: unknown[]): void;
 }
 
 /**
- * <en_us>Interface: asynchronous method</en_us>
+ * <en_us>interface: asynchronous method</en_us>
  * <zh_cn>接口：异步方法</zh_cn>
- * <zh_tw>介面：异步方法</zh_tw>
+ * <zh_tw>接口：異步方法</zh_tw>
  */
 export interface AsyncFunc extends Function {
 	async(...args: unknown[]): void;
 }
 
 /**
- * <en_us>Guard: Asynchronous method</en_us>
+ * <en_us>Guard: asynchronous method</en_us>
  * <zh_cn>守卫：异步方法</zh_cn>
- * <zh_tw>守衛：异步方法</zh_tw>
- * @param func {unknown} <en_us>en_us</en_us><zh_cn>需检测是否为异步方法的方法</zh_cn><zh_tw>zh_tw</zh_tw>
- * @returns {boolean} <en_us>en_us</en_us><zh_cn>检测结果，true表示该方法为异步方法，false表示该方法为同步方法</zh_cn><zh_tw>zh_tw</zh_tw>
+ * <zh_tw>守衛：異步方法</zh_tw>
+ * @param func {unknown} <en_us>Methods that need to be asynchronous</en_us><zh_cn>需检测是否为异步方法的方法</zh_cn><zh_tw>需檢測是否為異步方法的方法</zh_tw>
+ * @returns {boolean} <en_us>detection results, True represents this method as asynchronous method, FALSE indicates that the method is a synchronization method</en_us><zh_cn>检测结果，true表示该方法为异步方法，false表示该方法为同步方法</zh_cn><zh_tw>檢測結果，true表示該方法為異步方法，false表示該方法為同步方法</zh_tw>
  */
 export function isAsyncFunc(func: unknown): func is AsyncFunc {
 	return func instanceof Function && (func instanceof Promise);
 }
 
 /**
- * <en_us>Guard: synchronization method</en_us>
+ * <en_us>Guard: Synchronous method</en_us>
  * <zh_cn>守卫：同步方法</zh_cn>
  * <zh_tw>守衛：同步方法</zh_tw>
- * @param func {unknown} <en_us>en_us</en_us><zh_cn>需检测是否为同步方法的方法</zh_cn><zh_tw>zh_tw</zh_tw>
- * @returns {boolean} <en_us>en_us</en_us><zh_cn>检测结果，true表示该方法为同步方法，false表示该方法为异步方法</zh_cn><zh_tw>zh_tw</zh_tw>
+ * @param func {unknown} <en_us>Methods that need to be detected as a synchronization method</en_us><zh_cn>需检测是否为同步方法的方法</zh_cn><zh_tw>需檢測是否為同步方法的方法</zh_tw>
+ * @returns {boolean} <en_us>detection results, True represents the method of synchronization, FALSE represents the asynchronous method</en_us><zh_cn>检测结果，true表示该方法为同步方法，false表示该方法为异步方法</zh_cn><zh_tw>檢測結果，true表示該方法為同步方法，false表示該方法為異步方法</zh_tw>
  */
 export function isSyncFunc(func: unknown): func is SyncFunc {
 	return func instanceof Function && !(func instanceof Promise);
 }
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>Internationalization: Provide international support basic types, marks, etc.</en_us>
 <zh_cn>国际化：提供国际化支持基础类型、标记等</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>國際化：提供國際化支持基礎類型、標記等</zh_tw>
 ================================================================================================ */
 
 /**
@@ -214,12 +214,12 @@ export function isSyncFunc(func: unknown): func is SyncFunc {
 export type I18N_LANG_KIND = 'en_us' | 'zh_cn' | 'zh_tw';
 
 /**
- * <en_us>internationalization</en_us>
+ * <en_us>to detect whether the string is supported by the supported international language type</en_us>
  * <zh_cn>检测字符串是否为所支持的国际化语言类型</zh_cn>
- * <zh_tw>可國際化</zh_tw>
+ * <zh_tw>檢測字符串是否為所支持的國際化語言類型</zh_tw>
  *
- * @param lang {string} <en_us>en_us</en_us><zh_cn>需检测是否为所支持的国际化语言的字符串</zh_cn><zh_tw>zh_tw</zh_tw>
- * @returns {boolean} <en_us>en_us</en_us><zh_cn>是否为所支持的国际化语言</zh_cn><zh_tw>zh_tw</zh_tw>
+ * @param lang {string} <en_us>need to detect the string of the internationalized language that is supported</en_us><zh_cn>需检测是否为所支持的国际化语言的字符串</zh_cn><zh_tw>需檢測是否為所支持的國際化語言的字符串</zh_tw>
+ * @returns {boolean} <en_us>Is it a supported international language</en_us><zh_cn>是否为所支持的国际化语言</zh_cn><zh_tw>是否為所支持的國際化語言</zh_tw>
  */
 export function isI18nLangKind(lang: string): lang is I18N_LANG_KIND {
 	return ['en_us', 'zh_cn', 'zh_tw'].indexOf(lang) > -1;
@@ -283,12 +283,12 @@ export interface I18nable {
 }
 
 /**
- * <en_us>internationalization</en_us>
+ * <en_us>to detect whether the string is supported by the supported international language type</en_us>
  * <zh_cn>检测字符串是否为所支持的国际化语言类型</zh_cn>
- * <zh_tw>可國際化</zh_tw>
+ * <zh_tw>檢測字符串是否為所支持的國際化語言類型</zh_tw>
  *
- * @param lang {string} <en_us>en_us</en_us><zh_cn>需检测是否为所支持的国际化语言的字符串</zh_cn><zh_tw>zh_tw</zh_tw>
- * @returns {boolean} <en_us>en_us</en_us><zh_cn>是否为所支持的国际化语言</zh_cn><zh_tw>zh_tw</zh_tw>
+ * @param lang {string} <en_us>need to detect the string of the internationalized language that is supported</en_us><zh_cn>需检测是否为所支持的国际化语言的字符串</zh_cn><zh_tw>需檢測是否為所支持的國際化語言的字符串</zh_tw>
+ * @returns {boolean} <en_us>Is it a supported international language</en_us><zh_cn>是否为所支持的国际化语言</zh_cn><zh_tw>是否為所支持的國際化語言</zh_tw>
  */
 export function isI18nable(obj: object): obj is I18nable {
 	return 'en_us' in obj && 'zh_cn' in obj && 'zh_tw' in obj;
@@ -351,41 +351,41 @@ export enum I18nFlag {
 }
 
 /**
- * <en_us></en_us>
+ * <en_us>Start the mark length</en_us>
  * <zh_cn>开始标记长度</zh_cn>
- * <zh_tw></zh_tw>
+ * <zh_tw>開始標記長度</zh_tw>
  */
 export const I18N_HTML_BEGIN_TAG_LENGTH = 7;
 
 /**
- * <en_us></en_us>
+ * <en_us>end the marker length</en_us>
  * <zh_cn>结束标记长度</zh_cn>
- * <zh_tw></zh_tw>
+ * <zh_tw>結束標記長度</zh_tw>
  */
 export const I18N_HTML_END_TAG_LENGTH = I18N_HTML_BEGIN_TAG_LENGTH + 1;
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>Digital type assistance</en_us>
 <zh_cn>数字类型辅助</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>數字類型輔助</zh_tw>
 ================================================================================================ */
 /**
- * <en_us>Advanced rounding</en_us>
+ * <en_us>High -level four houses and five entry</en_us>
  * <zh_cn>高级四舍五入</zh_cn>
  * <zh_tw>高級四捨五入</zh_tw>
  *
  * @param num {number}
- * <en_us>raw number</en_us>
+ * <en_us>original number</en_us>
  * <zh_cn>原始数字</zh_cn>
  * <zh_tw>原始數字</zh_tw>
  *
  * @param digit {number}
- * <en_us>Number of digits</en_us>
+ * <en_us>digits</en_us>
  * <zh_cn>位数</zh_cn>
  * <zh_tw>位數</zh_tw>
  *
  * @returns {number}
- * <en_us>Round results</en_us>
+ * <en_us>Four House and Five Entry Results</en_us>
  * <zh_cn>四舍五入结果</zh_cn>
  * <zh_tw>四捨五入結果</zh_tw>
  */
@@ -394,8 +394,8 @@ export const roundWithDigit = (num: number, digit: number) => parseFloat(num.toF
 declare global {
 	interface Number {
 		/**
-		 * <en_us>en_us</en_us><zh_cn>带小数位进行四舍五入</zh_cn><zh_tw>zh_tw</zh_tw>
-		 * @param digit {number} <en_us>en_us</en_us><zh_cn>小数位数</zh_cn><zh_tw>zh_tw</zh_tw>
+		 * <en_us>with a decimal position for four houses and five entry</en_us><zh_cn>带小数位进行四舍五入</zh_cn><zh_tw>帶小數位進行四捨五入</zh_tw>
+		 * @param digit {number} <en_us>decimal digits</en_us><zh_cn>小数位数</zh_cn><zh_tw>小數位數</zh_tw>
 		 * @returns {number} 带小数位四舍五入结果
 		 */
 		_roundWithDigit(digit: number): number;
@@ -407,15 +407,15 @@ Number.prototype._roundWithDigit = function (digit: number): number {
 };
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>file processing, unified DENO and node</en_us>
 <zh_cn>文件处理，统一deno与node</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>文件處理，統一deno與node</zh_tw>
 ================================================================================================ */
 export {
 	/**
-        <en_us>en_us</en_us>
+        <en_us>merger path</en_us>
         <zh_cn>合并路径</zh_cn>
-        <zh_tw>zh_tw</zh_tw>
+        <zh_tw>合併路徑</zh_tw>
         @see https://deno.land/std@0.204.0/path/mod.ts
     */
 	join as joinPath,
@@ -432,23 +432,23 @@ export {
 } from 'https://deno.land/std@0.211.0/path/separator.ts';
 
 /**
- * <en_us>en_us</en_us>
+ * <en_us>complete permissions write file mode</en_us>
  * <zh_cn>完整权限写入文件模式</zh_cn>
- * <zh_tw>zh_tw</zh_tw>
+ * <zh_tw>完整權限寫入文件模式</zh_tw>
  */
 export const FILE_MODE_ALL = { mode: 0o777 };
 
 /**
-    <en_us>en_us</en_us>
+    <en_us>Full permissions new file mode</en_us>
     <zh_cn>完整权限新建文件模式</zh_cn>
-    <zh_tw>zh_tw</zh_tw>
+    <zh_tw>完整權限新建文件模式</zh_tw>
 */
 export const FILE_CREATE_NEW_AND_MODE_ALL = { createNew: true, mode: 0o777 };
 
 /* ================================================================================================
-<en_us></en_us>
+<en_us>debug</en_us>
 <zh_cn>调试</zh_cn>
-<zh_tw></zh_tw>
+<zh_tw>調試</zh_tw>
 ================================================================================================ */
 export {
 	type AnyConstructor, // Any constructor
@@ -485,9 +485,9 @@ export {
 } from 'https://deno.land/std@0.211.0/assert/mod.ts';
 
 /* ================================================================================================
-<en_us>Provide process processing related functions </en_us>
+<en_us>Provide process processing related functions, uniformly write DENO and NODE, and currently obtain related items from lib.deno.ns.d.ts, and then give a few items to a small number</en_us>
 <zh_cn>提供进程处理相关功能，统一deno与node的写法，目前从lib.deno.ns.d.ts获取相关项，再给少数项取别名 </zh_cn>
-<zh_tw>提供進程處理相關功能</zh_tw>
+<zh_tw>提供進程處理相關功能，統一deno與node的寫法，目前從lib.deno.ns.d.ts獲取相關項，再給少數項取別名 </zh_tw>
 ================================================================================================ */
 export const NotFoundError = Deno.errors.NotFound;
 export const PermissionDeniedError = Deno.errors.PermissionDenied;
@@ -591,16 +591,16 @@ export type Server = Deno.HttpServer;
 
 export const {
 	/**
-	 * 	<en_us>en_us</en_us>
+	 * 	<en_us>Process ID</en_us>
 	 * 	<zh_cn>进程id</zh_cn>
-	 * 	<zh_tw>zh_tw</zh_tw>
+	 * 	<zh_tw>進程id</zh_tw>
 	 */
 	pid: PROCESS_ID,
 
 	/**
-	 * 	<en_us>en_us</en_us>
+	 * 	<en_us>Father's process ID</en_us>
 	 * 	<zh_cn>父进程id</zh_cn>
-	 * 	<zh_tw>zh_tw</zh_tw>
+	 * 	<zh_tw>父進程id</zh_tw>
 	 */
 	ppid: PROCESS_PARENT_ID,
 
@@ -627,17 +627,17 @@ export const {
 	execPath,
 
 	/**
-	 * <en_us></en_us>
+	 * <en_us>Set the current activity directory</en_us>
 	 * <zh_cn>设置当前活动目录</zh_cn>
-	 * <zh_tw></zh_tw>
+	 * <zh_tw>設置當前活動目錄</zh_tw>
 	 */
 	chdir: setCurrentWorkingDirectory,
 
 	/**
-	 * <en_us></en_us>
+	 * <en_us>Get the current activity directory</en_us>
 	 * <zh_cn>获取当前活动目录</zh_cn>
-	 * <zh_tw></zh_tw>
-	 * @returns {string} <en_us>en_us</en_us><zh_cn>当前活动目录</zh_cn><zh_tw>zh_tw</zh_tw>
+	 * <zh_tw>獲取當前活動目錄</zh_tw>
+	 * @returns {string} <en_us>Current activity directory</en_us><zh_cn>当前活动目录</zh_cn><zh_tw>當前活動目錄</zh_tw>
 	 */
 	cwd: getCurrentWorkingDirectory,
 	link,
